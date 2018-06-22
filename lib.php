81f3c592959b0eb04e74a694638f053e95d80a1d
vagrant <vagrant@vagrant.vm>
2018-06-22 16:09:05 +0000

    First commit

---
+++ lib.php
@@ -0,0 +1,142 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The enrol plugin mmbr is defined here.
+ *
+ * @package     enrol_mmbr
+ * @copyright   2018 DmitryN defrakcija123@gmail.com
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// The base class 'enrol_plugin' can be found at lib/enrollib.php. Override
+// methods as necessary.
+
+/**
+ * Class enrol_mmbr_plugin.
+ */
+class enrol_mmbr_plugin extends enrol_plugin {
+
+    /**
+     * Add new instance of enrol plugin with default settings.
+     * @param object $course
+     * @return int id of new instance
+     */
+    public function add_default_instance($course) {
+        $fields = $this->get_instance_defaults();
+        return $this->add_instance($course, $fields);
+    }
+
+        /**
+     * Does this plugin allow manual enrolments?
+     *
+     * @param stdClass $instance course enrol instance
+     * All plugins allowing this must implement 'enrol/xxx:enrol' capability
+     *
+     * @return bool - true means user with 'enrol/xxx:enrol' may enrol others freely, false means nobody may add more enrolments manually
+     */
+    public function allow_enrol(stdClass $instance) {
+        return false;
+    }
+    /**
+     * Does this plugin allow manual unenrolment of all users?
+     * All plugins allowing this must implement 'enrol/xxx:unenrol' capability
+     *
+     * @param stdClass $instance course enrol instance
+     * @return bool - true means user with 'enrol/xxx:unenrol' may unenrol others freely, false means nobody may touch user_enrolments
+     */
+    public function allow_unenrol(stdClass $instance) {
+        return false;
+    }
+    /**
+     * Does this plugin allow manual unenrolment of a specific user?
+     * All plugins allowing this must implement 'enrol/xxx:unenrol' capability
+     *
+     * This is useful especially for synchronisation plugins that
+     * do suspend instead of full unenrolment.
+     *
+     * @param stdClass $instance course enrol instance
+     * @param stdClass $ue record from user_enrolments table, specifies user
+     *
+     * @return bool - true means user with 'enrol/xxx:unenrol' may unenrol this user, false means nobody may touch this user enrolment
+     */
+    public function allow_unenrol_user(stdClass $instance, stdClass $ue) {
+        return $this->allow_unenrol($instance);
+    }
+    /**
+     * Does this plugin allow manual changes in user_enrolments table?
+     *
+     * All plugins allowing this must implement 'enrol/xxx:manage' capability
+     *
+     * @param stdClass $instance course enrol instance
+     * @return bool - true means it is possible to change enrol period and status in user_enrolments table
+     */
+    public function allow_manage(stdClass $instance) {
+        return false;
+    }
+
+    /**
+     * Use the standard interface for adding/editing the form.
+     *
+     * @since Moodle 3.1.
+     * @return bool.
+     */
+    public function use_standard_editing_ui() {
+        return true;
+    }
+
+    /**
+     * Adds form elements to add/edit instance form.
+     *
+     * @since Moodle 3.1.
+     * @param object $instance Enrol instance or null if does not exist yet.
+     * @param MoodleQuickForm $mform.
+     * @param context $context.
+     * @return void
+     */
+    public function edit_instance_form($instance, MoodleQuickForm $mform, $context) {
+        // Do nothing by default.
+    }
+
+    /**
+     * Perform custom validation of the data used to edit the instance.
+     *
+     * @since Moodle 3.1.
+     * @param array $data Array of ("fieldname"=>value) of submitted data.
+     * @param array $files Array of uploaded files "element_name"=>tmp_file_path.
+     * @param object $instance The instance data loaded from the DB.
+     * @param context $context The context of the instance we are editing.
+     * @return array Array of "element_name"=>"error_description" if there are errors, empty otherwise.
+     */
+    public function edit_instance_validation($data, $files, $instance, $context) {
+        // No errors by default.
+        debugging('enrol_plugin::edit_instance_validation() is missing. This plugin has no validation!', DEBUG_DEVELOPER);
+        return array();
+    }
+
+    /**
+     * Return whether or not, given the current state, it is possible to add a new instance
+     * of this enrolment plugin to the course.
+     *
+     * @param int $courseid.
+     * @return bool.
+     */
+    public function can_add_instance($courseid) {
+        return true;
+    }
+}
